30b49d5dc6e944720720bbd2317083e0108e74cc
A_Nightails <a.nightails@gmail.com>
2025-05-29 14:55:43 -0500

    Add geting characters count, sorting, displaying, and path to book argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-from stats import get_num_words
+from sys import argv
+from stats import get_num_words, get_num_characters, sort_characters
 
 def get_book_text(file_path):
     contents = ""
@@ -7,8 +8,30 @@ def get_book_text(file_path):
         contents = file.read()
     return contents
 
+def print_sorted_characters(sorted_character):
+    for char in sorted_character:
+        if char["char"].isalpha():
+            print(f"{char['char']}: {char['count']}")
+
 def main():
-    words = get_num_words(get_book_text("books/frankenstein.txt"))
-    print(f"{words} words found in the document")
+    if len(argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+        return
+
+    file_path = argv[1]
+    book_content = get_book_text(file_path)
+
+    words_count = get_num_words(book_content)
+    characters_count = get_num_characters(book_content)
+    sorted_characters = sort_characters(characters_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words")
+    print("--------- Character Count -------")
+    print_sorted_characters(sorted_characters)
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
 
 def get_num_words(text):
     return len(text.split())
+
+def get_num_characters(text):
+    characters = dict()
+
+    for char in text:
+        if char.lower() not in characters:
+            characters[char.lower()] = 1
+        else:
+            characters[char.lower()] += 1
+
+    return characters
+
+def sort_characters(characters_dict):
+    sorted_characters = list()
+    characters = list(characters_dict)
+    for char in characters:
+        c = {"char": char, "count": characters_dict[char]}
+        sorted_characters.append(c)
+    sorted_characters.sort(key=lambda x: x['count'], reverse=True)
+    return sorted_characters
